c56defc6bb6f85b1f55e5b7a1c7472a54df32452
KosieVich <junkjuk5645@gmail.com>
2023-11-20 16:21:58 +0200

    Create user model and user controller

---
+++ lab1back/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using lab1back.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab1back.Controllers;
+
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet("users")]
+    public IActionResult GetUsers()
+    {
+        try
+        {
+            return Ok(new HealthcheckResponse(""));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(new HealthcheckResponse("Error"));
+        }
+    }
+    
+    [HttpGet("user/{id}")]
+    public IActionResult Get(Guid id)
+    {
+        try
+        {
+            return Ok(new HealthcheckResponse(id.ToString()));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(new HealthcheckResponse("Error"));
+        }
+    }
+
+    [HttpDelete("user/{id}")]
+    public IActionResult Delete(Guid id)
+    {
+        try
+        {
+            return Ok(new HealthcheckResponse(id.ToString()));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(new HealthcheckResponse("Error"));
+        }
+    }
+
+    [HttpPost("user")]
+    public IActionResult Delete([FromBody]string name)
+    {
+        try
+        {
+            return Ok(new HealthcheckResponse(name));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(new HealthcheckResponse("Error"));
+        }
+    }
+}
---
+++ lab1back/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace lab1back.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
