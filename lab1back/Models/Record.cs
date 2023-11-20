d1aea621d2e7bcb938c161431df8cf53101e43c0
KosieVich <junkjuk5645@gmail.com>
2023-11-20 16:22:19 +0200

    Add record and category model

---
+++ lab1back/Models/Record.cs
@@ -0,0 +1,10 @@
+﻿namespace lab1back.Models;
+
+public class Record
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public Guid CategoryId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public float Amount { get; set; }
+}
