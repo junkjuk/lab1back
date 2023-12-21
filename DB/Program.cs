9768ddb38a4e9d6c3459f6db0d5d08966ce9411e
KosieVich <junkjuk5645@gmail.com>
2023-12-21 23:01:01 +0200

    remove test cod on db

--- DB/Program.cs
+++ DB/Program.cs
@@ -1,16 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using DB;
-using lab1back.Models;
-
-var fack = new AppContextFactory();
-var context = fack.CreateDbContext(new string[] { });
-
-Console.WriteLine("start");
-await context.Set<Category>().AddAsync(new Category {Id = Guid.NewGuid(), Name = "test2"});
-await context.SaveChangesAsync();
-foreach (var cat in context.Set<Category>())
-{
-    Console.WriteLine(cat.Name);
-}
 Console.WriteLine("end");
